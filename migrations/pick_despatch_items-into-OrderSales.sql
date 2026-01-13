3089a9fb3348db187db751d637966e0d66b2f905
chrislydiat-collab <chris.lydiat@powertoolworld.co.uk>
2026-01-13 10:10:01 +0000

    Fixing Order Date Migration Issue

--- migrations/pick_despatch_items-into-OrderSales.sql
+++ migrations/pick_despatch_items-into-OrderSales.sql
@@ -1,86 +1,36 @@
-INSERT INTO [lw].[Order_sales] (
-    OrderDate,
-    ItemID,
-    UnitCost,
-    TotalCost,
-    UnitPrice,
-    TotalPrice,
-    UnitTax,
-    TotalTax,
-    TotalIncTax,
-    Quantity,
-    SubItemSKU,
-    SubItemUnitCost,
-    SubItemQty,
-    fkOrderId,
-    FkLocationId,
-    ItemSource,
-    fkStockItemId,
-    Source
+;WITH corder AS (
+    SELECT DISTINCT
+        CAST([Sales_Order_Num] AS VARCHAR(50)) AS [OrderKey], 
+        [Order Date] AS [OrderDate]
+    FROM [MaginusOMS].[dbo].[fct_Sales_Orders]
+)
+INSERT INTO [linnworks].[lw].[Order_sales] (
+    OrderDate, ItemID, UnitCost, TotalCost, UnitPrice, TotalPrice, 
+    UnitTax, TotalTax, TotalIncTax, Quantity, SubItemSKU, 
+    SubItemUnitCost, SubItemQty, fkOrderId, FkLocationId, 
+    ItemSource, fkStockItemId, [Source], DispatchDate
 )
 SELECT
-    -- OrderDate (convert DateKey to datetime)
-    CONVERT(DATETIME, 
-            STUFF(STUFF(CAST(fs.DateKey AS CHAR(8)), 5, 0, '/'), 8, 0, '/') + ' 00:00:00') AS OrderDate,
-
-    -- ItemID: use Product_Code instead of ProductKey
-    dp.Product_Code AS ItemID,
-
-    -- UnitCost = ABS(Total Cost) / Quantity
-    CASE WHEN fs.Quantity <> 0 
-         THEN ABS(fs.[Total Cost]) / fs.Quantity 
-         ELSE 0 END AS UnitCost,
-
-    -- TotalCost = ABS(Total Cost)
-    ABS(fs.[Total Cost]) AS TotalCost,
-
-    -- UnitPrice = Base Line Value / Quantity
-    CASE WHEN fs.Quantity <> 0 
-         THEN fs.[Base Line Value] / fs.Quantity 
-         ELSE 0 END AS UnitPrice,
-
-    -- TotalPrice = Base Line Value
-    fs.[Base Line Value] AS TotalPrice,
-
-    -- UnitTax = Base Line VAT Value / Quantity
-    CASE WHEN fs.Quantity <> 0 
-         THEN fs.[Base Line VAT Value] / fs.Quantity 
-         ELSE 0 END AS UnitTax,
-
-    -- TotalTax = Base Line VAT Value
-    fs.[Base Line VAT Value] AS TotalTax,
-
-    -- TotalIncTax = Base Gross Line Value
-    fs.[Base Gross Line Value] AS TotalIncTax,
-
-    -- Quantity
-    fs.Quantity,
-
-    -- SubItemSKU = N/A
-    NULL AS SubItemSKU,
-
-    -- SubItemUnitCost = N/A
-    NULL AS SubItemUnitCost,
-
-    -- SubItemQty = N/A
-    NULL AS SubItemQty,
-
-    -- fkOrderId
-    fs.Sales_Order_No AS fkOrderId,
-
-    -- FkLocationId
-    fs.WarehouseKey AS FkLocationId,
-
-    -- ItemSource = N/A
-    NULL AS ItemSource,
-
-    -- fkStockId = N/A
-    NULL AS fkStockItemId,
-
-    -- Source
-    'maginus' AS Source
+    fso.[OrderDate],                                                         
+    dp.Product_Code,                                                         
+    CASE WHEN fs.Quantity <> 0 THEN ABS(fs.[Total Cost]) / fs.Quantity ELSE 0 END, 
+    ABS(fs.[Total Cost]),                                                    
+    CASE WHEN fs.Quantity <> 0 THEN fs.[Base Line Value] / fs.Quantity ELSE 0 END, 
+    fs.[Base Line Value],                                                    
+    CASE WHEN fs.Quantity <> 0 THEN fs.[Base Line VAT Value] / fs.Quantity ELSE 0 END, 
+    fs.[Base Line VAT Value],                                                
+    fs.[Base Gross Line Value],                                              
+    fs.[Quantity],                                                           
+    NULL,                                                                    
+    NULL,                                                                    
+    NULL,                                                                    
+    fs.Sales_Order_No,                                                       
+    fs.WarehouseKey,                                                         
+    NULL,                                                                    
+    NULL,                                                                    
+    'maginus',                                                               
+    TRY_CONVERT(DATETIME, CAST(fs.DateKey AS CHAR(8)), 112)                  
 FROM [MaginusOMS].[dbo].[fct_Sales] fs
-INNER JOIN [MaginusOMS].[dbo].[Dim_Product] dp
-    ON fs.ProductKey = dp.ProductKey;
-
-
+LEFT JOIN corder fso ON CAST(fs.Sales_Order_No AS VARCHAR(50)) = fso.OrderKey
+INNER JOIN [MaginusOMS].[dbo].[Dim_Product] dp ON fs.ProductKey = dp.ProductKey;
+GO
